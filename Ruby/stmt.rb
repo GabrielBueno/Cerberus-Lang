7789617da83b5cb6baa1a537c7dca1c1bb97e896
GabrielBueno <gabrielbueno99@outlook.com>
2019-10-24 20:26:25 -0300

    Arvore sintatica para if
    Implementacao dos metodos no parser para a construcao de uma arvore sintatica que contempla estruturas condicionais

--- Ruby/stmt.rb
+++ Ruby/stmt.rb
@@ -1,6 +1,8 @@
+# Statement genérico
 class Stmt
 end
 
+# Declaração de variável
 class AssignmentStmt < Stmt
     attr_accessor :assignment_op
     attr_accessor :identifier
@@ -17,6 +19,7 @@ class AssignmentStmt < Stmt
     end
 end
 
+# Print
 class PrintStmt < Stmt
     attr_accessor :expr
 
@@ -26,5 +29,98 @@ class PrintStmt < Stmt
 
     def accept(visitor)
         visitor.visit_print(self)
+    end
+end
+    
+# Statement de expressões
+class ExprStmt < Stmt
+    attr_accessor :expr
+
+    def initialize(expr)
+        @expr = expr
+    end
+
+    def accept(visitor)
+        visitor.visit_expr(self)
+    end
+end
+
+# If statement
+class IfStmt < Stmt
+    attr_accessor :expr
+    attr_accessor :block
+    attr_accessor :else_stmt
+    attr_accessor :elif_stmts
+
+    def initialize(expr, block)
+        @expr  = expr
+        @block = block
+
+        @elif_stmts = []
+        @else_stmt  = nil
+    end
+
+    def add_elif(elif_stmt)
+        @elif_stmts.push(elif_stmt)
+    end
+
+    def set_else(else_stmt)
+        @else_stmt = else_stmt
+    end
+
+    def accept(visitor)
+        visitor.visit_if(self)
+    end
+
+    def has_else?
+        @else_stmt != nil
+    end
+
+    def has_elif?
+        @elif_stmts.length > 0
+    end
+
+    # Else stmt
+    class Else
+        attr_accessor :block
+
+        def initialize(block)
+            @block = block
+        end
+
+        def accept(visitor)
+            visitor.visit_else(self)
+        end
+    end
+
+    # Elif stmt
+    class Elif
+        attr_accessor :expr
+        attr_accessor :block
+
+        def initialize(expr, block)
+            @expr  = expr
+            @block = block
+        end
+
+        def accept(visitor)
+            visitor.visit_elif(self)
+        end
+    end
+end
+
+class Block
+    attr_accessor :stmts
+
+    def initialize()
+        @stmts = []
+    end
+
+    def add_stmt(stmt)
+        @stmts.push(stmt)
+    end
+
+    def accept(visitor)
+        visitor.visit_block(self)
     end
 end
